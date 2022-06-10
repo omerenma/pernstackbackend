c5dd417d784a2b40bf286336a9486cc1e1f39c53
omerenma <godwin2341@gmail.com>
2022-06-10 17:01:08 +0100

    change password

--- db/index.js
+++ db/index.js
@@ -1,22 +1,18 @@
 const { Pool } = require("pg");
 
-const isProduction = process.env.NODE_ENV === "production";
+const isProduction = process.env.NODE_ENV === "production"
 
-const connectionString = `postgresql://${process.env.PGUSER} : ${process.env.PGPASSWORD}@${process.env.PGHOST}:${process.env.PGPORT}/${process.env.PGDATABASE}`;
+const connectionString = `postgresql://${process.env.PGUSER} : ${process.env.PGPASSWORD}@${process.env.PGHOST}:${process.env.PGPORT}/${process.env.PGDATABASE}`
 
 const pool = new Pool({
-	user: "postgres",
-	host: "localhost",
-	database: "yelp",
-	password: "omerenma1",
-	port: 5432,
-	// connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+	connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
 	ssl:{
 		rejectUnauthorized:false
 	}
+	
 });
 
 module.exports = {
 	query: (text, params) => pool.query(text, params),
-	pool: pool,
+	pool:pool
 };
