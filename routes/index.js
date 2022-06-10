0f239a369efc11e0302c7ead3183342ae05e9f63
omerenma <godwin2341@gmail.com>
2022-06-10 20:34:22 +0100

    test

--- routes/index.js
+++ routes/index.js
@@ -4,20 +4,20 @@ const db = require("../db");
 
 // Get all restaurants
 
-router.get("/restaurants", async(req, res) => {
-	const {rows} = await db.query("SELECT * FROM restaurants")
-	if(rows.length > 0){
-		res.status(200).json(rows)
-	}else{
-		res.status(200).json({message:"Something went wrong"})
-	}
-	
+router.get("/restaurants", async (req, res) => {
+	// const {rows} = await db.query("SELECT * FROM restaurants")
+	// if(rows.length > 0){
+	// 	res.status(200).json(rows)
+	// }else{
+	// 	res.status(200).json({message:"Something went wrong"})
+	// }
+	res.send("Hello World");
 });
 
 // Get a restaurant
 router.get("/:id", async (req, res) => {
 	const { id } = req.params;
-	
+
 	const { rows } = await db.query("SELECT * FROM restaurants WHERE id = $1", [
 		id,
 	]);
@@ -26,8 +26,10 @@ router.get("/:id", async (req, res) => {
 
 // Post a restaurant
 
-router.post("/", (req, res) => {
-	const { name, location } = req.body;
+router.post("/", async(req, res) => {
+	
+const result = await db.query('INSERT INTO restaurants (name, location, price_range) values("Chicken", "Gombe", 5)')
+res.send(result)
 });
 
 // Edit a restaurant
