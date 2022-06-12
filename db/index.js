87a86653b9dd0c46007d475c50c21dc4667f85d0
omerenma <godwin2341@gmail.com>
2022-06-12 18:38:45 +0100

    fixed database connection

--- db/index.js
+++ db/index.js
@@ -1,21 +1,22 @@
 const { Pool, Client } = require("pg");
 require('dotenv').config()
 
-const isProduction = process.env.DATABASE_URL
+const isProduction = process.env.NODE_ENV
 
 const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
 
 const pool = new Pool({
-	connectionString: process.env.NODE_ENV === 'production' ? isProduction : connectionString,
-	ssl: {
-		rejectUnauthorized: false,
-	},
+	connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+	// ssl: {
+	// 	rejectUnauthorized: false,
+	// },
+	ssl:isProduction
 });
 // pool.connect();
 
 
 
 module.exports = {
-	query: (text, params) => pool.query(text, params),
-	pool: pool,
+	// query: (text, params) => pool.query(text, params),
+	pool
 };
