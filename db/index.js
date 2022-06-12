9d9ecabdbd7543170c09bc55d6bf7cacbfdb6556
omerenma <godwin2341@gmail.com>
2022-06-12 15:44:09 +0100

    fixed database connection

--- db/index.js
+++ db/index.js
@@ -4,15 +4,15 @@ const isProduction = process.env.NODE_ENV === "production";
 
 const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
 
-const client = new Client({
+const pool = new Pool({
 	connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
-	// ssl: {
-	// 	rejectUnauthorized: false,
-	// },
+	ssl: {
+		rejectUnauthorized: false,
+	},
 });
-client.connect();
+pool.connect();
 
 module.exports = {
 	query: (text, params) => client.query(text, params),
-	client: client,
+	pool: pool,
 };
