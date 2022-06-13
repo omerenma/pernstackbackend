919371c556c04d1d01e50f352531f3a20df0f25e
omerenma <godwin2341@gmail.com>
2022-06-13 13:22:56 +0100

    fixed database connection

--- routes/index.js
+++ routes/index.js
@@ -4,26 +4,28 @@ const db = require("../db");
 
 // Get all restaurants
 
-router.get('/', (req, res) =>{
-	res.send('Welcome to our CRUD')
-})
+router.get("/", (req, res) => {
+	res.send("Welcome to our CRUD");
+});
 router.get("/restaurants", async (req, res) => {
-	 db.pool.query("SELECT * FROM restaurants")
-	 .then(data =>{
-		 res.send(data)
-	 }).catch(err => {
-		 res.send('err')
-	 })
-	
+	db.pool
+		.query("SELECT * FROM restaurants")
+		.then((data) => {
+			res.json(data.rows);
+		})
+		.catch((err) => {
+			res.send("err");
+		});
 });
 
 // Get a restaurant
 router.get("/restaurants/:id", async (req, res) => {
 	const { id } = req.params;
 
-	const { rows } = await db.query("SELECT * FROM restaurants WHERE id = $1", [
-		id,
-	]);
+	const { rows } = await db.pool.query(
+		"SELECT * FROM restaurants WHERE id = $1",
+		[id]
+	);
 	res.status(200).json(rows);
 });
 
@@ -31,19 +33,54 @@ router.get("/restaurants/:id", async (req, res) => {
 
 router.post("/restaurants", async (req, res) => {
 	const { name, location, price_range } = req.body;
+
 	const insert =
-		"INSERT INTO restaurants(name, location, price_range) VALUES($1, $2, $3)";
+		"INSERT INTO restaurants(name, location, price_range) VALUES($1, $2, $3) returning * ";
 	const values = [name, location, price_range];
 
-	const result = await db.query(insert, values);
-	res.send(result.rowCount);
+	await db.pool
+		.query(insert, values)
+		.then((data) => {
+			res.json({
+				messsage: "Restaurant successfully added",
+				data: data.rows,
+			});
+		})
+		.catch((err) => {
+			res.send(err.messsage);
+		});
 });
 
 // Edit a restaurant
 
-router.put("/:id", (req, res) => {});
+router.put("/restaurants/:id", async (req, res) => {
+	try {
+		const { name, location, price_range } = req.body;
+		const { id } = req.params;
+		const update =
+			"UPDATE restaurants  SET name = $1, location = $2, price_range = $3 WHERE id = $4 returning *";
+		const values = [name, location, price_range, id];
+		const result = await db.pool.query(update, values);
+		res.send(result.rows[0]);
+	} catch (error) {
+		res.send(error.messsage);
+	}
+});
 
 // Delete a restaurant
 
-router.delete("/", (req, res) => {});
+router.delete("/restaurants/:id", async (req, res) => {
+	try {
+		const { id } = req.params;
+		const deleteRecord = `DELETE FROM restaurants WHERE id = ${id} `;
+		const result = await db.pool.query(deleteRecord);
+		if (result.rowCount === 1) {
+			res.json({
+				messsage: "Record successfully deleted",
+			});
+		}
+	} catch (error) {
+		res.send(error.messsage);
+	}
+});
 module.exports = router;
