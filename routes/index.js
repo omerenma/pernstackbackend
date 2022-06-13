c77977e002bf4240735503f46a15233abc58a4bb
omerenma <godwin2341@gmail.com>
2022-06-13 14:10:49 +0100

    fixed database connection

--- routes/index.js
+++ routes/index.js
@@ -10,9 +10,9 @@ const postgresRouter = new Router();
 router.get("/", (req, res) => {
 	res.send("Welcome to our CRUD");
 });
-router.get("/restaurants", async (req, res) => {
+router.get("/restaurants",  (req, res) => {
 	const select = "SELECT * FROM restaurants"
-	const {rows} = await db.query(select)
+	const {rows} =  db.query(select)
 	res.send(rows)
 		
 });
