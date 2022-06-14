733ef7b3dca8393838069833d9a9120c04c569bd
omerenma <godwin2341@gmail.com>
2022-06-14 13:23:13 +0100

    deploy

--- db/index.js
+++ db/index.js
@@ -1,12 +1,12 @@
 const { Pool, Client } = require("pg");
 require("dotenv").config();
 
-const isProduction = process.env.NODE_ENV;
+const isProduction = process.env.NODE_ENV === 'production'
 
-const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
+const connectionString = `postgresql://${process.env.PGUSER}:${process.env.PGPASSWORD}@${process.env.PGHOST}:${process.env.PGPORT}/${process.env.PGDATABASE}`;
 
 const pool = new Pool({
-	connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+	connectionString: isProduction ? process.env.HEROKU_POSTGRESQL_JADE_URL : connectionString,
 	ssl: {
 		rejectUnauthorized:  false
 	},
