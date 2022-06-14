1213455e1142255236d6f28dd5c2a4e893d26cac
omerenma <godwin2341@gmail.com>
2022-06-14 13:26:02 +0100

    deploy

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.json());
 
 app.use("/api/v1", router);
 
-const port = process.env.PORT;
+const port = process.env.PORT || 4000
 
 app.listen(port, () => {
 	console.log("Servers running on port" + " " + port);
