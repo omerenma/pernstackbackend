33b9d27bb5a29b0ccde32461edde8a321faa780a
omerenma <godwin2341@gmail.com>
2022-06-15 12:03:20 +0100

    get reviews by id

--- routes/reviews.js
+++ routes/reviews.js
@@ -2,17 +2,30 @@ const express = require("express");
 const router = express.Router();
 const db = require("../db");
 
+// Select sql
 
 // get reviews
 
-router.get('/reviews', async (req, res) =>{
-    const select_reviews = 'SELECT * FROM reviews';
-    try {
-        const result = await db.query(select_reviews)
-        res.json(result.rows)
-    } catch (error) {
-        res.send(error.message)
-    } 
-})
+router.get("/reviews", async (req, res) => {
+	const select_reviews = "SELECT * FROM reviews";
+	try {
+		const result = await db.query(select_reviews);
+		res.json(result.rows);
+	} catch (error) {
+		res.send(error.message);
+	}
+});
 
-module.exports = router
+// Get review for a particular restaurants
+router.get("/reviews/:id", async (req, res) => {
+	const { restaurants_id } = req.params;
+	const select_reviews = `SELECT * FROM reviews WHERE id = ${restaurants_id}  `;
+	try {
+		const result = await db.query(select_reviews);
+        res.json(result)
+	} catch (error) {
+        res.send(error.message)
+    }
+});
+
+module.exports = router;
