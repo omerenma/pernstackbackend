4da4b1a8156d71f50f89b2ed07d53ee6a957e464
omerenma <godwin2341@gmail.com>
2022-06-17 10:51:04 +0100

    add express validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,31 +1,50 @@
 const router = require("express").Router();
+const { body, validationResult, check } = require("express-validator");
 const db = require("../db");
 const bcrypt = require("bcryptjs");
 const jwt_generator = require("../utils/jwt_generator");
 
 // Register user
-router.post("/register", async (req, res) => {
-	try {
-		const { name, email, password } = req.body;
-		const hashPassword = await bcrypt.hash(password, 10);
-		const check = await db.query("SELECT * FROM users WHERE email = $1", [
-			email,
-		]);
-		if (check.rows.length > 0) {
-			return res
-				.status(400)
-				.json({ message: "User with this email already exist" });
+router.post(
+	"/register",
+	check("email").isEmail().withMessage("Not a valid email"),
+	check("password")
+		.isLength({ min: 7 })
+		.withMessage("Password length must be greater 6")
+		.matches(/\d/)
+		.withMessage("Must contain a number"),
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			errors.errors.map((error) => {
+				res.status(400).json({
+					message: error.msg,
+				});
+			});
+			return;
 		}
-		const insert =
-			"INSERT INTO users(name, email, password) VALUES ($1, $2, $3) returning *";
-		const value = [name, email, hashPassword];
-		const newUser = await db.query(insert, value);
-		const token = jwt_generator(newUser.rows.id);
-		res.status(201).json({ token });
-	} catch (error) {
-		res.status(500).json({ message: "Internal server error" });
+		try {
+			const { name, email, password } = req.body;
+			const hashPassword = await bcrypt.hash(password, 10);
+			const check = await db.query("SELECT * FROM users WHERE email = $1", [
+				email,
+			]);
+			if (check.rows.length > 0) {
+				return res
+					.status(400)
+					.json({ message: "User with this email already exist" });
+			}
+			const insert =
+				"INSERT INTO users(name, email, password) VALUES ($1, $2, $3) returning *";
+			const value = [name, email, hashPassword];
+			const newUser = await db.query(insert, value);
+			const token = jwt_generator(newUser.rows.id);
+			res.status(201).json({ token });
+		} catch (error) {
+			res.status(500).json({ message: "Internal server error" });
+		}
 	}
-});
+);
 
 // Login user
 router.post("/login", async (req, res) => {
