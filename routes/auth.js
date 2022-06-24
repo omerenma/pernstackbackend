46be56f58f6740e811fa737e59cc78e9e97c550d
omerenma <godwin2341@gmail.com>
2022-06-24 13:10:47 +0100

    edit login route

--- routes/auth.js
+++ routes/auth.js
@@ -63,11 +63,14 @@ router.post("/login", async (req, res) => {
 		const { email, password } = req.body;
 		const select = "SELECT * FROM users WHERE email = $1";
 		const value = [email];
-		await db.query(select, value);
-		if (user.rows.length === 0) {
-			res.json({ message: "Invalid email" });
-		}
-		const passwordMatch = await bcrypt.compare(password, user.rows[0].password);
+	
+			const user = await db.query(select, value);
+
+			if (user.rows.length === 0) {
+			return res.status(401).json({ message: "Invalid credential" });
+
+			}
+			const passwordMatch = await bcrypt.compare(password, user.rows[0].password);
 
 		if (passwordMatch) {
 			const token = jwt_generator(user.rows[0]);
