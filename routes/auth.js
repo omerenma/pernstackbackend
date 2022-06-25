e766624586301aa9d42eb839876e1e0d656dd319
omerenma <godwin2341@gmail.com>
2022-06-25 21:20:44 +0100

    add cors

--- routes/auth.js
+++ routes/auth.js
@@ -63,7 +63,7 @@ router.post("/login", async (req, res) => {
 	const select = "SELECT * FROM users WHERE email = $1";
 	const value = [email];
 	console.log(req.body.email, "email req", req.body.password, "req password");
-	await db.query(select, req.body.email).then((user) => {
+	await db.query(select, [req.body.email]).then((user) => {
 		console.log(user.rows, "user");
 		if (user.rows.length === 0) {
 			return res.status(402).json({ message: "Invalid credential" });
