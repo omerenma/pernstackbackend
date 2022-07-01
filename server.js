b14f06b46e4804e24715520490be21034ab87ed4
omerenma <godwin2341@gmail.com>
2022-07-01 09:54:16 +0100

    refactor server.js

--- server.js
+++ server.js
@@ -1,26 +1,32 @@
 const express = require("express");
 require("dotenv").config();
 const app = express();
-const cors = require('cors')
+const cors = require("cors");
 const bodyParser = require("body-parser");
 // Restaurants routes
 const restaurants = require("./routes/index");
 // Review routes
 const reviews = require("./routes/reviews");
-const auth = require('./routes/auth')
+const auth = require("./routes/auth");
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
-app.use(bodyParser.urlencoded({extended:true}))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.use("/api/v1", restaurants);
 app.use("/api/v1", reviews);
 // Register and Login route
-app.use('/api/v1/auth', auth)
+app.use("/api/v1/auth", auth);
 
-const port = process.env.PORT || 4000;
+const serverStart = () => {
+	try {
+		app.listen(process.env.PORT, () => {
+			console.log(`server running on port ${port}`);
+		});
+	} catch (error) {
+		console.log(error.message);
+	}
+};
 
-app.listen(port, () => {
-	console.log("Servers running on port" + " " + port);
-});
+serverStart();
