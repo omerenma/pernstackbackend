17a8aaf06052ac51e9dee3afb8cb4df406ccce85
omerenma <godwin2341@gmail.com>
2022-07-22 12:32:47 +0100

    add restaurant file upload

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ const path = require("path");
 
 const storage = multer.diskStorage({
 	destination: (req, file, cb) => {
-		cb(null, '../images' );
+		cb(null, 'images' );
 	},
 	filename: (req, file, cb) => {
 		console.log(file);
