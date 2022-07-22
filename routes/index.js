bb7ca719b83763423bc9982d9d33f8a5d8ddacc0
omerenma <godwin2341@gmail.com>
2022-07-22 16:05:39 +0100

    add restaurant file upload

--- routes/index.js
+++ routes/index.js
@@ -52,6 +52,7 @@ router.get("/restaurants/:id", async (req, res) => {
 router.post("/restaurants", upload.single('image'), async (req, res) => {
 
 	const filepath = req.file.path
+	console.log(filepath, 'file path')
 
 	const { name, location, price_range } = req.body;
 
